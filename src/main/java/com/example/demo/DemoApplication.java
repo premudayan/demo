933c70a7d5649f54e63fb6a5c488e431bb39f6eb
UPrem <prem_udayan@yahoo.com>
2021-01-13 16:25:36 -0500

    Created a class Student Controller  that server as the API layer

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,18 +1,9 @@
 package com.example.demo;
 
-import com.example.demo.Student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.stylesheets.LinkStyle;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 
@@ -20,15 +11,5 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(
-                new Student( 1L,
-                        "prem@hotmail.com",
-                        "Prem",
-                        LocalDate.of(2000, Month.APRIL, 10),
-                        30
-                        )
-        );
-    }
+
 }
