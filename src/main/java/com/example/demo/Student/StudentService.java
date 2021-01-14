777d00432d486a63aeb4c95d229d7258a0341074
UPrem <prem_udayan@yahoo.com>
2021-01-14 10:43:42 -0500

    Implemented Update student API

--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -3,7 +3,9 @@ package com.example.demo.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 // the below annotation can be a component or service
@@ -36,5 +38,33 @@ public class StudentService {
             throw new IllegalStateException("Student with Id " + studentId + " does not exist !");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+
+        System.out.println("Test ----  1");
+
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Student with id "+ studentId + " does not exist !"
+                ));
+        System.out.println("Test ----  2");
+
+        if ( name != null && name.trim().length() > 0 && !Objects.equals(student.getName(), name ) ) {
+            student.setName(name);
+        }
+
+        System.out.println("Test ----  3");
+        if ( email != null && email.trim().length() > 0 && !Objects.equals(student.getEmail(), email ) ) {
+
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if ( studentOptional.isPresent()) {
+                throw new IllegalStateException("Email Taken !");
+            }
+            student.setEmail(email);
+        }
+        System.out.println("Test ----  4");
+
     }
 }
